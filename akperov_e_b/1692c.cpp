198592cde03bba3de4414b98a70d26f1b9920955
emilakper <113722793+emilakper@users.noreply.github.com>
2022-10-15 12:24:57 +0300

    Update and rename 1692c_incorrect.cpp to 1692c.cpp

---
+++ akperov_e_b/1692c.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int t;
+    std::cin >> t;
+    int i;
+    int j;
+    std::string x;
+    std::string z = "#";
+    int xi;
+    int xj;
+    bool f;
+    while (t--) {
+        f = true;
+        for (i = 0; i < 8; i++) {
+            std::cin >> x;
+            for (j = 0; j < 6; j++) {
+                if (x[j] == '#' && x[j + 2] == '#') {
+                    xi = i;
+                    xj = j + 2;
+                }
+            }
+
+        }
+        std::cout << xi << " " << xj << "\n";
+    }
+}
