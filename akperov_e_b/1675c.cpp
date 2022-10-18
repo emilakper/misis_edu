163cf6f62af898ee4047889d504b2797e2d2f320
emilakper <113722793+emilakper@users.noreply.github.com>
2022-10-18 11:56:50 +0300

    Update 1675c.cpp

--- akperov_e_b/1675c.cpp
+++ akperov_e_b/1675c.cpp
@@ -15,15 +15,17 @@ int main() {
         std::cin >> x;
         for (int i = 0; i < x.length(); ++i) {
             if (x[i] == '1') {
-                right1 = i - 1;
+                right1 = i;
             }
             if (x[i] == '0' && f) {
-                left0 = i + 1;
+                left0 = x.length() - i-1;
                 f = false;
             }
         }
         std::cout << x.length() - left0 - right1 << "\n";
+
     }
 }
+
 
 
